f81689a1550fe993874fab84083a6704bbdb6514
Romain Lancelot <rlancelot@myges.fr>
2023-05-11 19:18:32 +0200

    create admin layout

---
+++ cookmasters-webapp/resources/views/layouts/admin-master.blade.php
@@ -0,0 +1,39 @@
+@routes
+@extends('layouts.partials.html-head')
+
+@section('styles')
+<link href="{!! url('assets/css/app.css') !!}" rel="stylesheet">
+@endsection
+
+@section('body')
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
+
+    <body>
+
+        @include('layouts.partials.navbar-admin')
+        @include('layouts.partials.messages')
+
+        <main class="container">
+            @yield('content')
+        </main>
+
+        @include('layouts.partials.footer')
+    </body>
+
+
+    @yield('scripts')
+@endsection
