c3a996f711ff0149806ff1eb553bd644f10a6e1c
Romain Lancelot <rlancelot@myges.fr>
2023-05-11 19:18:09 +0200

    change extends layout

--- cookmasters-webapp/resources/views/auth/login.blade.php
+++ cookmasters-webapp/resources/views/auth/login.blade.php
@@ -1,35 +1,36 @@
-@extends('layouts.auth-master')
+@extends('layouts.app-master')
 
 @section('title', 'Login')
 
 @section('content')
-    <form method="post" action="{{ route('login.perform') }}">
-        
-        <input type="hidden" name="_token" value="{{ csrf_token() }}" />
-        <img class="mb-4" src="{!! url('images/cookmaster-logo.png') !!}" alt="" width="72" height="57">
-        
-        <h1 class="h3 mb-3 fw-normal">Login</h1>
+    <div class="text-center mt-4">
+        <form method="post" action="{{ route('login.perform') }}">
 
-        @include('layouts.partials.messages')
+            <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+            <img class="mb-4" src="{!! url('images/cookmaster-logo.png') !!}" alt="" width="72" height="57">
 
-        <div class="form-group form-floating mb-3">
-            <input type="text" class="form-control" name="username" value="{{ old('username') }}" placeholder="Username" required="required" autofocus>
-            <label for="floatingName">Email or Username</label>
-            @if ($errors->has('username'))
-                <span class="text-danger text-left">{{ $errors->first('username') }}</span>
-            @endif
-        </div>
-        
-        <div class="form-group form-floating mb-3">
-            <input type="password" class="form-control" name="password" value="{{ old('password') }}" placeholder="Password" required="required">
-            <label for="floatingPassword">Password</label>
-            @if ($errors->has('password'))
-                <span class="text-danger text-left">{{ $errors->first('password') }}</span>
-            @endif
-        </div>
+            <h1 class="h3 mb-3 fw-normal">Login</h1>
 
-        <button class="btn btn-lg btn-primary" type="submit">Login</button>
-        
-        @include('auth.partials.copy')
-    </form>
+            @include('layouts.partials.messages')
+
+            <div class="form-group form-floating mb-3">
+                <input type="text" class="form-control" name="username" value="{{ old('username') }}" placeholder="Username" required="required" autofocus>
+                <label for="floatingName">Email or Username</label>
+                @if ($errors->has('username'))
+                    <span class="text-danger text-left">{{ $errors->first('username') }}</span>
+                @endif
+            </div>
+
+            <div class="form-group form-floating mb-3">
+                <input type="password" class="form-control" name="password" value="{{ old('password') }}" placeholder="Password" required="required">
+                <label for="floatingPassword">Password</label>
+                @if ($errors->has('password'))
+                    <span class="text-danger text-left">{{ $errors->first('password') }}</span>
+                @endif
+            </div>
+
+            <button class="btn btn-lg btn-primary" type="submit">Login</button>
+
+        </form>
+    </div>
 @endsection
