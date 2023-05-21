ad0465b2077eaf4c49e5d52f84f5d953b0317d9c
Romain Lancelot <rlancelot@myges.fr>
2023-05-21 18:25:15 +0200

    add multiple language

---
+++ cookmasters-webapp/resources/lang/en/index.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'title' => 'Cookmasters',
+    'description' => 'Welcome to Cookmasters, the collaborative cooking recipe website!'
+];
--- cookmasters-webapp/resources/views/home/index.blade.php
+++ cookmasters-webapp/resources/views/home/index.blade.php
@@ -4,13 +4,13 @@
 @section('content')
     <div class="bg-light p-5 rounded">
         @auth
-        <h1>Dashboard</h1>
+        <h1>{{ __('index.title') }}</h1>
         <p class="lead">Only authenticated users can access this section.</p>
         @endauth
 
         @guest
-        <h1>Homepage</h1>
-        <p class="lead">Your viewing the home page. Please login to view the restricted data.</p>
+        <h1>{{ __('index.title') }}</h1>
+        <p class="lead">{{ __('index.description') }}</p>
         @endguest
     </div>
 @endsection
