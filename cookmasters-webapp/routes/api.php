df888d57225dd50ccae51cccc5b4de34bd937843
Romain Lancelot <rlancelot@myges.fr>
2023-06-19 19:22:08 +0200

    bugfix name routes api

--- cookmasters-webapp/routes/api.php
+++ cookmasters-webapp/routes/api.php
@@ -14,33 +14,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-
-/**
- * Auth Routes
- */
-Route::post('/login', 'App\Http\Controllers\API\AuthController@login')->name('login');
-
-/**
- * Protected Routes
- */
-
-Route::group(['middleware' => 'auth.api'], function () {
+Route::group(['namespace' => 'App\Http\Controllers\API'], function()
+{
     /**
-     * Logout
+     * Auth Routes
      */
-    Route::post('/logout', 'App\Http\Controllers\API\AuthController@logout')->name('logout');
+    Route::post('/login', 'AuthController@login');
 
     /**
-     * Get user account informations
+     * Protected Routes
      */
-    Route::post('/account', 'App\Http\Controllers\API\AuthController@account')->name('account');
 
-    /**
-     * get users list
-     */
-    Route::get('/users', 'App\Http\Controllers\API\UsersController@index')->name('users.index');    
+    Route::group(['middleware' => 'auth.api'], function () {
+        /**
+         * Logout
+         */
+        Route::post('/logout', 'AuthController@logout');
+
+        /**
+         * Get user account informations
+         */
+        Route::post('/account', 'AuthController@account');
+
+        /**
+         * get users list
+         */
+        Route::get('/users', 'UsersController@index');    
+    });
 });
