edafe43451deedd35e5d50d31451f6589be7598f
YAHIA ABDCHAFEE ADAM <adam.y.abdc@gmail.com>
2023-06-26 20:23:52 +0200

    add home page about us contact us and workshop

--- requeteur/build/config.d
+++
@@ -1,5 +0,0 @@
-build/config.o: src/database/config.c /usr/include/mysql/mysql.h \
- /usr/include/mysql/field_types.h /usr/include/mysql/my_list.h \
- /usr/include/mysql/mysql_com.h /usr/include/mysql/my_command.h \
- /usr/include/mysql/my_compress.h /usr/include/mysql/mysql_version.h \
- /usr/include/mysql/mysql_time.h /usr/include/mysql/errmsg.h
