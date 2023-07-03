07f3a80a616fac0eb0b1170ba581b2103fdae069
Romain Lancelot <rlancelot@myges.fr>
2023-07-03 16:26:23 +0200

    change cooking recipe presentation

---
+++ cookmasters-webapp/public/assets/js/cooking-recipes/dataTables.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let table = new DataTable('#recipes-table', {
+        language: {
+            url: '//cdn.datatables.net/plug-ins/1.13.4/i18n/fr-FR.json'
+        },
+        columnDefs: [
+            { "targets": [0,6], "orderable": false },
+            // { "targets": 5, "orderable": false },
+        ],
+        order: [[ 1, 'asc' ]]
+    });
+});
--- cookmasters-webapp/resources/views/cooking-recipes/index.blade.php
+++ cookmasters-webapp/resources/views/cooking-recipes/index.blade.php
@@ -2,27 +2,65 @@
 
 @section('title', 'Cooking recipes')
 
+@section('styles')
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
+@endsection
+
 @section('content')
     <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
         <h1 class="display-4 fw-normal">Cooking Recipes</h1>
         <p class="fs-5 text-body-secondary">Explore our collection of cooking recipes and discover new tasty meals.</p>
     </div>
-    <a class="btn btn-primary" href="{{ route('cooking-recipes.create') }}">Create new Cooking recipe</a>
+    @if (auth()->user()->role_name() == 'admin')
+        <a class="btn btn-primary mb-5" href="{{ route('cooking-recipes.create') }}">Create new Cooking recipe</a>
+    @endif
+    <table class="table table-striped table-hover display" id="recipes-table">
+        <thead>
+            <th scope="col">Image</th>
+            <th scope="col">Nom</th>
+            <th scope="col">Description</th>
+            <th scope="col">Temps de préparation</th>
+            <th scope="col">Personnes</th>
+            <th scope="col">Difficulté</th>
+            <th scope="col">Actions</th>
+        </thead>
+        <tbody>
+            @foreach ($recipes as $recipe)
+                <tr>
+                    <td scope="row">
+                        @if ($recipe->image)
+                            <img src="{{ asset($recipe->image) }}" alt="{{ $recipe->name }}" class="img-fluid" width="100">
+                        @else
+                            No image
+                        @endif
+                    </td>
+                    <td>{{ $recipe->name }}</td>
+                    <td>{{ $recipe->description }}</td>
+                    <td>{{ $recipe->cooking_time }} minutes</td>
+                    <td>{{ $recipe->people }}</td>
+                    <td>{{ $recipe->difficulty }} / 10</td>
+                    <td>
+                        <a href="{{ route('cooking-recipes.show', $recipe->id) }}" class="btn btn-primary">Voir la recette</a>
+                        @if (auth()->user()->role_name() == 'admin')
+                            <a href="{{ route('cooking-recipes.edit', $recipe->id) }}" class="btn btn-secondary">Modifier</a>
+                            <form action="{{ route('cooking-recipes.destroy', $recipe->id) }}" method="POST" class="d-inline">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-danger">Supprimer</button>
+                            </form>
+                        @endif
+                    </td>
+                    </div>
+                </div>
+            @endforeach
+        </tbody>
+    </table>
 
-    @foreach ($recipes as $recipe)
-        <div class="card mb-3">
-            <div class="card-body">
-                <h5 class="card-title">{{ $recipe->name }}</h5>
-                <p class="card-text">{{ $recipe->description }}</p>
-                <a href="{{ route('cooking-recipes.show', $recipe->id) }}" class="btn btn-primary">View</a>
-                <a href="{{ route('cooking-recipes.edit', $recipe->id) }}" class="btn btn-secondary">Edit</a>
-                <form action="{{ route('cooking-recipes.destroy', $recipe->id) }}" method="POST" class="d-inline">
-                    @csrf
-                    @method('DELETE')
-                    <button class="btn btn-danger">Delete</button>
-                </form>
-            </div>
-        </div>
-    @endforeach
+    @section('scripts')
+        <script type="text/javascript" src="{{ secure_asset('assets/js/cooking-recipes/dataTables.js') }}"></script>
+        <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+    @endsection
 
 @endsection
