4bef7eb377a7f7fcf0b85083c1653dd8c9efbd9d
Romain Lancelot <rlancelot@myges.fr>
2023-07-04 09:38:34 +0200

    add cookmasters tab icon

--- cookmasters-webapp/resources/views/layouts/partials/html-head.blade.php
+++ cookmasters-webapp/resources/views/layouts/partials/html-head.blade.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>@yield('title')</title>
+        <link rel="icon" href="{{ secure_asset('images/cookmaster-logo.png') }}" type="image/x-icon"/>
         <link href="{{ secure_asset('assets/css/app.css') }}" rel="stylesheet">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
