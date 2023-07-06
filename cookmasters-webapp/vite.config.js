9b04e6887d26d9f3bbaf7ab3ab8e73b67f9d4faa
Romain Lancelot <rlancelot@myges.fr>
2023-07-06 13:41:16 +0200

    bugfix conf

--- cookmasters-webapp/vite.config.js
+++ cookmasters-webapp/vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
             input: [
                 'resources/sass/app.scss',
                 'resources/js/app.js',
+                'resources/js/conversations.js',
             ],
             refresh: true,
         }),
