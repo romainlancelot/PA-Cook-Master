36f36236359ce6b87f987dc69138a65c96011ce1
Julien <sage.julien@gmail.com>
2023-07-06 23:57:17 +0200

    recup de data de l api 3

--- cookmasters-javaapp/src/main/java/fr/cookmasters/javaapp/ChartsController.java
+++ cookmasters-javaapp/src/main/java/fr/cookmasters/javaapp/ChartsController.java
@@ -7,7 +7,8 @@ import javafx.scene.chart.PieChart;
 import com.google.gson.*;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
-
+import java.time.LocalDate;
+import java.time.Period;
 import java.io.IOException;
 
 public class ChartsController {
@@ -15,14 +16,17 @@ public class ChartsController {
     private PieChart camenbert;
 
     @FXML
+    private PieChart camenbert1;
+    @FXML
     private BarChart<String, Number> age;
 
     @FXML
-    private BarChart<String, Number> revenu;
+    private BarChart<String, Number> vraiAge;
 
     @FXML
     private Button btnCamenbert;
     int pro, free, basic;
+    int user, admin;
     // String path = "C:\\Users\\sagej\\OneDrive\\Documents\\ESGI\\2ème année\\2ème Semestre\\Java\\Projets\\APPLIJAVAPA2\\json\\data.json";
 
     private ApiConnection api = null;
@@ -47,21 +51,91 @@ public class ChartsController {
      */
     @FXML
     void generate(ActionEvent Event) throws IOException {
+        //remettre les graph a 0:
         camenbert.getData().clear();
+        camenbert1.getData().clear();
         age.getData().clear();
+        //remettre les var a 0:
+        pro = 0;
+        free = 0;
+        basic = 0;
+        user = 0;
+        admin = 0;
+/*
+        vraiAge.getData().clear();
+*/
 
         try {
             JsonObject users = api.getUsers();
-            System.out.println(users);
             JsonElement dataElement = users.get("data");
-            JsonElement nameElement = users.get("firstname");
-
-            JsonElement spElement = users.get("subscription_plan_id");
             JsonArray dataArray = JsonParser.parseString(String.valueOf(dataElement)).getAsJsonArray();
             for (JsonElement element:dataArray) {
                 JsonObject dataList = element.getAsJsonObject();
+
+                /////////////////////////////////////////////
+                //recup le prenom de l'user :
                 String spFirstname = dataList.get("firstname").getAsString();
-                System.out.println(spFirstname);
+                System.out.print("\n" + spFirstname + " ");
+
+
+                /////////////////////////////////////////////
+                //recup le nom de l'user :
+                String spLastname = dataList.get("lastname").getAsString();
+                System.out.println(spLastname);
+
+
+                /////////////////////////////////////////////
+                //recup l'email de l'user :
+                String spEmail = dataList.get("email").getAsString();
+                System.out.println("email : " + spEmail);
+
+
+                /////////////////////////////////////////////
+                //recup l'username de l'user :
+                String spUsername = dataList.get("username").getAsString();
+                System.out.println("username : " + spUsername);
+
+
+                /////////////////////////////////////////////
+                //recup le pays de l'user :
+                JsonElement countryElement = dataList.get("country");
+                if (countryElement != null  && !(countryElement instanceof JsonNull)) {
+                    String spPays = dataList.get("country").getAsString();
+                    System.out.println("pays : " + spPays);
+                } else {
+                    // La valeur est nulle, faire quelque chose en conséquence
+                    System.out.println("Pays inconnu.");
+                }
+
+
+                /////////////////////////////////////////////
+                //recup l'age avec la birthday de l'user :
+                JsonElement birthdayElement = dataList.get("birthday");
+                Integer years = null;
+                // Vérifier si la valeur n'est pas nulle
+                if (birthdayElement != null && !(birthdayElement instanceof JsonNull)) {
+                    String birthdayString = birthdayElement.getAsString();
+                    LocalDate birthday = LocalDate.parse(birthdayString);
+
+                    // Calculer l'âge
+                    LocalDate currentDate = LocalDate.now();
+                    Period age = Period.between(birthday, currentDate);
+                    years = age.getYears();
+
+                    // Utiliser l'âge
+                    Integer spAge = years;
+                    System.out.println("date de naissance : " + birthday);
+                    System.out.println("age : " + years + " ans.");
+                } else {
+                    // La valeur est nulle, faire quelque chose en conséquence
+                    System.out.println("Date de naissance inconnue.");
+                    System.out.println("age inconnu.");
+                }
+                /////////////////////////////////////////////
+
+
+                /////////////////////////////////////////////
+                //recup le type de subscription de l'user :
                 int spData = dataList.get("subscription_plan_id").getAsInt();
                 switch (spData) {
                     case 1: free++;
@@ -71,11 +145,39 @@ public class ChartsController {
                     case 3: pro++;
                     break;
                 }
-            }
-            System.out.println(free);
-            System.out.println(basic);
-            System.out.println(pro);
+                /////////////////////////////////////////////
 
+                /////////////////////////////////////////////
+                //recup le role de l'user :
+                String roleName = dataList.get("role").getAsJsonObject().get("name").getAsString();
+                System.out.println("role : " + roleName);
+                switch (roleName) {
+                    case "admin": admin++;
+                        break;
+                    case "user": user++;
+                        break;
+                }
+                /////////////////////////////////////////////
+            }
+
+            //////////////////////////////////////////////////////////////////////////////////////////
+            /////////////////////////////////////////////
+            //print le type de subscription de l'user :
+            System.out.println("\n");
+            System.out.println("roleName :");
+            System.out.println("admin : " + admin);
+            System.out.println("user : " + user);
+            System.out.println("\n");
+
+            /////////////////////////////////////////////
+            //print le type de role de l'user :
+            System.out.println("subscriptionPlanType :");
+            System.out.println("free : " + free);
+            System.out.println("basic : " + basic);
+            System.out.println("pro : " + pro);
+            System.out.println("\n");
+
+            //subscription id camenbert
             PieChart.Data freeData = new PieChart.Data("free", free);
             PieChart.Data basicData = new PieChart.Data("basic", basic);
             PieChart.Data proData = new PieChart.Data("pro", pro);
@@ -84,6 +186,16 @@ public class ChartsController {
             camenbert.getData().add(basicData);
             camenbert.getData().add(proData);
 
+            //roleName camenbert
+            PieChart.Data userData = new PieChart.Data("user", user);
+            PieChart.Data adminData = new PieChart.Data("admin", admin);
+
+            camenbert1.getData().add(userData);
+            camenbert1.getData().add(adminData);
+
+
+
+
             //nombre de ID
             XYChart.Series identifiant = new XYChart.Series();
             identifiant.setName("ID");
@@ -91,21 +203,20 @@ public class ChartsController {
                 JsonObject idElement = element.getAsJsonObject();
                 int id = idElement.get("id").getAsInt();
                 String idString = idElement.get("id").getAsString();
-                System.out.println(id);
                 identifiant.getData().add(new XYChart.Data(idString, id));
             }
 
             age.getData().add(identifiant);
 
-            //revenus
+            /*//vraiAge
             XYChart.Series revenus = new XYChart.Series();
-            revenus.setName("Revenus");
+            revenus.setName("AGE");
 
             for (JsonElement element:dataArray){
                 JsonObject dataList = element.getAsJsonObject();
-                System.out.println(spElement);
             }
 
+            vraiAge.getData().add(identifiant);*/
         }
 
         catch(Exception E){
