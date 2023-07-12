7c0f3533d5e177337e8d905a2571eb8a52415b9c
Romain Lancelot <rlancelot@myges.fr>
2023-07-12 23:50:59 +0200

    bugfix

--- cookmasters-androidapp/app/src/main/java/com/example/cookmasters/LoginActivity.java
+++ cookmasters-androidapp/app/src/main/java/com/example/cookmasters/LoginActivity.java
@@ -1,42 +1,53 @@
 package com.example.cookmasters;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        EditText usernameEditText = findViewById(R.id.username);
-        EditText passwordEditText = findViewById(R.id.password);
 
-
+        usernameEditText = findViewById(R.id.username);
+        passwordEditText = findViewById(R.id.password);
         Button submitButton = findViewById(R.id.submit);
+
+        sharedPreferences = getSharedPreferences("my_prefs", Context.MODE_PRIVATE);
 
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String username = usernameEditText.getText().toString();
                 String password = passwordEditText.getText().toString();
-                SharedPreferences sharedPreferences = getSharedPreferences("my_prefs", Context.MODE_PRIVATE);
+
                 SharedPreferences.Editor editor = sharedPreferences.edit();
                 editor.putString("username", username);
                 editor.putString("password", password);
                 editor.apply();
 
-                Toast.makeText(LoginActivity.this, "Sign in successful", Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(LoginActivity.this, Cours.class));
             }
         });
@@ -45,15 +56,11 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        EditText usernameEditText = findViewById(R.id.username);
-        EditText passwordEditText = findViewById(R.id.password);
 
-        SharedPreferences sharedPreferences = getSharedPreferences("my_prefs", Context.MODE_PRIVATE);
         String savedUsername = sharedPreferences.getString("username", "");
         String savedPassword = sharedPreferences.getString("password", "");
 
         usernameEditText.setText(savedUsername);
         passwordEditText.setText(savedPassword);
     }
-
 }
