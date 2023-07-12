753957a19d465885d78ffb81760b62f28157aa05
Romain Lancelot <rlancelot@myges.fr>
2023-07-12 20:06:58 +0200

    remove comments

--- cookmasters-webapp/app/Models/CoursesModule.php
+++ cookmasters-webapp/app/Models/CoursesModule.php
@@ -44,12 +44,10 @@ class CoursesModule extends Model
 
     public function nextModule(Courses $course)
     {
-        // get where previous_module = current_module_id and courses_id = course_id 
         $next = CoursesModule::where('previous_module_id', $this->id)
             ->where('courses_id', $course->id)
             ->first();
         
         return $next != null ? $next->id : $this->id;
-
     }
 }
